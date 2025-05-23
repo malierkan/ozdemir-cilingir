4e1761bacb564454f1f0b8bee109566fc15d699e
Mali Erkan <malierkandenizli@gmail.com>
2025-05-23 15:05:58 +0300

    gtm is removed

--- public/js/analytics-loader.js
+++ public/js/analytics-loader.js
@@ -1,22 +1,22 @@
 function loadAnalytics() {
 	// 1) Global site tag (gtag.js) yükle
-	var gtagScript = document.createElement('script');
-	gtagScript.async = true;
-	gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=AW-16795811620';
-	document.head.appendChild(gtagScript);
+	// var gtagScript = document.createElement('script');
+	// gtagScript.async = true;
+	// gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=AW-16795811620';
+	// document.head.appendChild(gtagScript);
 
-	// 2) Config ve Conversion kodu yükle
-	gtagScript.onload = function () {
-		window.dataLayer = window.dataLayer || [];
-		function gtag() { dataLayer.push(arguments); }
-		window.gtag = gtag;
+	// // 2) Config ve Conversion kodu yükle
+	// gtagScript.onload = function () {
+	// 	window.dataLayer = window.dataLayer || [];
+	// 	function gtag() { dataLayer.push(arguments); }
+	// 	window.gtag = gtag;
 
-		gtag('js', new Date());
-		gtag('config', 'AW-16795811620');
-		gtag('event', 'conversion', {
-			'send_to': 'AW-16795811620/38p5CJ-zh7waEKT-7sg-'
-		});
-	};
+	// 	gtag('js', new Date());
+	// 	gtag('config', 'AW-16795811620');
+	// 	gtag('event', 'conversion', {
+	// 		'send_to': 'AW-16795811620/38p5CJ-zh7waEKT-7sg-'
+	// 	});
+	// };
 }
 
 // Daha önce çerezler kabul edildiyse hemen çalıştır
